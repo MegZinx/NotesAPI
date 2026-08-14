3f13a113957e29eeb32b4375a77f08d891e7b878
Maaz <maaz.zahid2006@gmail.com>
2026-08-14 11:31:07 +0500

    added views and routed urls

--- notesAPI/views.py
+++ notesAPI/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Note
+from .serializers import NoteSerializer
+from .permissions import IsOwner
 
 # Create your views here.
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "content"]
+
+    def get_queryset(self):
+        return Note.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
